09d9f41cacddce3219caf6ed8400319cffd41d09
caochi1 <153147473+caochi1@users.noreply.github.com>
2024-07-10 21:46:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	type ao struct{
-		name string
-	}
-	a := []ao{ao{"asd"}, ao{"dsa"}}
-	b := []ao{}
-	for _, v := range a {
-		b = append(b, v)
-	}
-	fmt.Println(b)
-	// models.Init(false)
-	// cache.Init()
-	// router.Router()
+
+	models.Init(false)
+	cache.Init()
+	router.Router()
 
 }
